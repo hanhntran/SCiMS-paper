37c2d14d541eb0bbb0882a5fdedf411c7ee695e0
hanhntran <hanhtran5213@gmail.com>
2025-03-17 18:32:25 -0400

    Create 04_simulation_scims.sh

---
+++ 01_simulation/04_simulation_scims.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Set directories consistent with Snakefile outputs
+IDXSTATS_DIR="../01_simulation/mapped_reads"
+OUTPUT_DIR="../01_simulation/results"
+SCAFFOLDS_FILE="../data/ref_genome/GRCh38_scaffolds.txt"
+METADATA_FILE="../data/simulation_metadata.tsv"
+THRESHOLD="0.5"
+X_scaffold="NC_000023.11"
+Y_scaffold="NC_000024.10"
+id_colummn="Run"
+
+
+# Create results directory
+mkdir -p ${OUTPUT_DIR}
+
+# Run SCiMS with simulated idxstats files
+scims \
+  --idxstats_folder ${IDXSTATS_DIR} \
+  --scaffolds ${SCAFFOLDS_FILE} \
+  --homogametic_scaffold ${X_scaffold} \
+  --heterogametic_scaffold ${Y_scaffold} \
+  --metadata ${METADATA_FILE} \
+  --id_column ${id_column} \
+  --output_dir ${OUTPUT_DIR}
+
