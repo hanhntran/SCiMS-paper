0a6bba9e6cc087719de04cf07c17bbd636acef03
hanhntran <hanhtran5213@gmail.com>
2025-03-17 20:00:04 -0400

    Update 02_hmp_scims.sh

---
+++ 02_hmp/02_hmp_scims.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set directories consistent with Snakefile outputs
+IDXSTATS_DIR="./02_hmp/mapped_reads"
+OUTPUT_DIR="./02_hmp/results"
+SCAFFOLDS_FILE="./data/ref_genome/GRCh38_scaffold.txt"
+METADATA_FILE="./02_hmp/dbGap_metadata.txt"
+X_scaffold="NC_000023.11"
+Y_scaffold="NC_000024.10"
+id_colummn="Run"
+
+# Run SCiMS
+scims \
+  --idxstats_folder ${IDXSTATS_DIR} \
+  --scaffolds ${SCAFFOLDS_FILE} \
+  --homogametic_scaffold ${X_scaffold} \
+  --heterogametic_scaffold ${Y_scaffold} \
+  --metadata ${METADATA_FILE} \
+  --id_column ${id_column} \
+  --output_dir ${OUTPUT_DIR}
