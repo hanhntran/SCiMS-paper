210e37d99362b71551529497d351aff42b37618c
hanhntran <hanhtran5213@gmail.com>
2025-08-19 18:59:29 -0400

    Change bexy command and add --maxNumThreads

--- 01_simulation/06_simulation_bexy.sh
+++ 01_simulation/06_simulation_bexy.sh
@@ -7,8 +7,5 @@ ls ${IDXSTATS_DIR}/*.1000x.idxstats > simulation_master.txt
 # run bexy
 scaffolds="data/ref_genome/GRCh38_scaffolds.txt"
 
-bexy infer --idxstats "simulation_master.txt" --keepScaffolds $scaffolds
-
-# generate bexy output in R
-Rscript ./01_simulation/bexy.R
+bexy infer --idxstats "simulation_master.txt" --keepScaffolds $scaffolds --maxNumThreads 16
 
