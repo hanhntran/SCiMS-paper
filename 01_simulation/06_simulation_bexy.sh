98fc96ef020327a8d1be52cd89b4e82ca58521d7
hanhntran <hanhtran5213@gmail.com>
2025-08-19 15:31:31 -0400

    Update 06_simulation_bexy.sh

--- 01_simulation/06_simulation_bexy.sh
+++ 01_simulation/06_simulation_bexy.sh
@@ -7,7 +7,7 @@ ls ${IDXSTATS_DIR}/*.1000x.idxstats > simulation_master.txt
 # run bexy
 scaffolds="data/ref_genome/GRCh38_scaffolds.txt"
 
-./tools/bexy/build/bexy infer --idxstats "simulation_master.txt" --keepScaffolds $scaffolds
+bexy infer --idxstats "simulation_master.txt" --keepScaffolds $scaffolds
 
 # generate bexy output in R
 Rscript ./01_simulation/bexy.R
