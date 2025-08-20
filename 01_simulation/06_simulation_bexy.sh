ea79c9ebd3b3b8311b89eeb6c689d03034233e62
Polina Tikhonova <tikhonova.polly@mail.ru>
2025-08-20 18:14:21 -0400

    01_simulation/06_simulation_bexy.sh
    01_simulation/06_simulation_bexy.sh

--- 01_simulation/06_simulation_bexy.sh
+++ 01_simulation/06_simulation_bexy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 IDXSTATS_DIR="./01_simulation/mapped_reads"
+OUTPUT_DIR="01_simulation/bexy_output"
 
-ls ${IDXSTATS_DIR}/*.1000x.idxstats > simulation_master.txt
+mkdir ${OUTPUT_DIR}
+
+ls ${IDXSTATS_DIR}/*.1000x.idxstats > ${OUTPUT_DIR}/simulation_master.txt
 
 # run bexy
 scaffolds="data/ref_genome/GRCh38_scaffolds.txt"
 
-bexy infer --idxstats "simulation_master.txt" --keepScaffolds $scaffolds --maxNumThreads 16
+./tools/bexy/build/bexy infer --idxstats "${OUTPUT_DIR}/simulation_master.txt" --keepScaffolds $scaffolds
 
+# generate bexy output in R
+Rscript ./scripts/bexy.R ${OUTPUT_DIR}/
