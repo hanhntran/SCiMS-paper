6246558c18f74b311bf26c6ad2f55a899675ce75
hanhntran <hanhtran5213@gmail.com>
2025-08-24 02:35:32 -0400

    Update 05_simulation_rxry.sh

--- 01_simulation/05_simulation_rxry.sh
+++ 01_simulation/05_simulation_rxry.sh
@@ -2,28 +2,24 @@
 
 # Variables
 THRESHOLD="0.95"
-RXRY_SCRIPT="./scripts/rxry_script.py"
-METADATA="./01_simulation/simulation_metadata.tsv"
+RXRY_SCRIPT="./scripts/calculate_rxry_script.py"
+#METADATA="./01_simulation/simulation_metadata.tsv"
 SCAFFOLDS="./01_simulation/simulated_ref/GRCh38_scaffolds.txt"
 OUTPUT_DIR="./01_simulation/results"
 IDXSTATS_DIR="./01_simulation/mapped_reads"
-MASTER_RXRY_OUTPUT="${OUTPUT_DIR}/simulation_rxry_output.txt"
+RXRY_OUTPUT="${OUTPUT_DIR}/simulation_rxry_output.txt"
 SYSTEM="XY"
 
 # Ensure output directory exists
 mkdir -p ${OUTPUT_DIR}
 
-# Generate master idxstats file list
-ls ${IDXSTATS_DIR}/*.1000x.idxstats > simulation_master.txt
-
 # Run RxRy analysis
 python3 ${RXRY_SCRIPT} \
     --scaffolds ${SCAFFOLDS} \
-    --metadata ${METADATA} \
-    --master_file simulation_master.txt \
+    --idxstats_dir ${IDXSTATS_DIR} \
     --system XY \
     --homogametic_id NC_000023.11 \
     --heterogametic_id NC_000024.10 \
-    --output ${MASTER_RXRY_OUTPUT} \
+    --output ${RXRY_OUTPUT} \
     --threshold ${THRESHOLD}
 
