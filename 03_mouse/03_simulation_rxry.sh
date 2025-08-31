bfd10532a9adeba7880f5c622892aa6508d4f376
hanhntran <hanhtran5213@gmail.com>
2025-08-30 20:58:12 -0400

    Update 03_simulation_rxry.sh

--- 03_mouse/03_simulation_rxry.sh
+++ 03_mouse/03_simulation_rxry.sh
@@ -3,26 +3,22 @@
 # Variables
 THRESHOLD="0.95"
 RXRY_SCRIPT="./scripts/rxry_script.py"
-METADATA="./03_mouse/mouse_metadata.tsv"
 SCAFFOLDS="./03_mouse/mouse_scaffold.txt"
 OUTPUT_DIR="./03_mouse/results"
 IDXSTATS_DIR="./03_mouse/mapped_reads"
-MASTER_RXRY_OUTPUT="${OUTPUT_DIR}/mouse_rxry_output.txt"
+RXRY_OUTPUT="${OUTPUT_DIR}/mouse_rxry_output.txt"
+SYSTEM="XY"
 
 # Ensure output directory exists
 mkdir -p ${OUTPUT_DIR}
 
-# Generate master idxstats file list
-ls ${IDXSTATS_DIR}/*.1000x.idxstats > mouse_idxstats.txt
-
 # Run RxRy analysis
 python3 ${RXRY_SCRIPT} \
     --scaffolds ${SCAFFOLDS} \
-    --metadata ${METADATA} \
-    --master_file mouse_idxstats.txt \
-    --system XY \
+    --idxstats_dir ${IDXSTATS_DIR} \
+    --system ${SYSTEM} \
     --homogametic_id CM001013.3 \
     --heterogametic_id CM001014.3 \
-    --output ${MASTER_RXRY_OUTPUT} \
+    --output ${RXRY_OUTPUT} \
     --threshold ${THRESHOLD}
 
