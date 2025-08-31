dfbde6aadb94a5115557dce6cf7353cc5377d713
hanhntran <hanhtran5213@gmail.com>
2025-08-30 22:52:25 -0400

    Create 04_hmp_bexy.sh

---
+++ 02_hmp/04_hmp_bexy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IDXSTATS_DIR="./02_hmp/mapped_reads"
+OUTPUT_DIR="./02_hmp/results/bexy"
+
+mkdir -p ${OUTPUT_DIR}
+
+ls ${IDXSTATS_DIR}/*.1000x.idxstats > ${OUTPUT_DIR}/hmp_idxstats.txt
+
+scaffolds="data/ref_genome/GRCh38_scaffolds.txt"
+
+# run bexy
+infer  --idxstats ${OUTPUT_DIR}/hmp_idxstats.txt --keepScaffolds $scaffolds --maxNumThreads 16
+
+
