216ff2764ff6f599e440ce87b7756f2c80f26809
Polina Tikhonova <tikhonova.polly@mail.ru>
2025-09-08 16:49:41 -0400

    fixed filenames listing;  03_mouse/04_mouse_bexy.sh

--- 03_mouse/04_mouse_bexy.sh
+++ 03_mouse/04_mouse_bexy.sh
@@ -5,7 +5,7 @@ OUTPUT_DIR="./03_mouse/results/bexy"
 
 mkdir -p ${OUTPUT_DIR}
 
-ls ${IDXSTATS_DIR}/*.1000x.idxstats > ${OUTPUT_DIR}/mouse_idxstats.txt
+ls ${IDXSTATS_DIR}/*.idxstats > ${OUTPUT_DIR}/mouse_idxstats.txt
 
 mouse_scaffolds="data/ref_genome/mouse_scaffolds.txt"
 
