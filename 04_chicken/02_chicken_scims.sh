0f4a20a19028ce05dd19323f7f479e3725c9da2e
hanhntran <hanhtran5213@gmail.com>
2025-09-10 10:33:40 -0400

    Update 02_chicken_scims.sh

--- 04_chicken/02_chicken_scims.sh
+++ 04_chicken/02_chicken_scims.sh
@@ -3,16 +3,16 @@ IDXSTATS_DIR="./04_chicken/mapped_reads"
 OUTPUT_DIR="./04_chicken/results"
 SCAFFOLDS_FILE="./data/ref_genome/chicken_scaffold.txt"
 METADATA_FILE="./04_chicken/chicken_metadata.tsv"
-X_scaffold="NC_000023.11"
-Y_scaffold="NC_000024.10"
+Z_scaffold="CM028522.1"
+W_scaffold="CM028521.1"
 id_colummn="Run"
 
 # Run SCiMS
 scims \
   --idxstats_folder ${IDXSTATS_DIR} \
   --scaffolds ${SCAFFOLDS_FILE} \
-  --homogametic_scaffold ${X_scaffold} \
-  --heterogametic_scaffold ${Y_scaffold} \
+  --homogametic_scaffold ${Z_scaffold} \
+  --heterogametic_scaffold ${W_scaffold} \
   --ZW \
   --metadata ${METADATA_FILE} \
   --id_column ${id_column} \
