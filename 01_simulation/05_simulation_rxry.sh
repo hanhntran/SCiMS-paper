721d43b9a39cd71fa2dc674c4646af1254a80b3c
hanhntran <hanhtran5213@gmail.com>
2026-09-10 11:58:56 -0400

    Update 05_simulation_rxry.sh

--- 01_simulation/05_simulation_rxry.sh
+++ 01_simulation/05_simulation_rxry.sh
@@ -2,11 +2,11 @@
 
 # Variables
 THRESHOLD="0.95"
-RXRY_SCRIPT="./scripts/calculate_rxry_script.py"
-SCAFFOLDS="./01_simulation/simulated_ref/GRCh38_scaffolds.txt"
-OUTPUT_DIR="./01_simulation/results"
-IDXSTATS_DIR="./01_simulation/mapped_reads"
-RXRY_OUTPUT="${OUTPUT_DIR}/simulation_rxry_output.txt"
+RXRY_SCRIPT="calculate_rxry_script.py"
+SCAFFOLDS="../GRCh38_scaffolds.txt"
+OUTPUT_DIR="results"
+IDXSTATS_DIR="../cami_idxstats"
+RXRY_OUTPUT="${OUTPUT_DIR}/hg38_cami_simulation_rxry_output.txt"
 SYSTEM="XY"
 
 # Ensure output directory exists
